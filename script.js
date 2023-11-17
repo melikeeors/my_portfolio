ad935a60b8c11da317593553a9a84a743d098e30
melikeeros <orsmelike2003@gmail.com>
2023-11-17 15:53:13 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,479 @@
+$(document).ready(function () {
+
+    $("#green").click(function () {
+        console.log("lala");
+        $("#body").css({
+            backgroundColor: "rgb(54, 84, 44)",
+        });
+        $("#headerImgBlue").css({
+            display: "none",
+        });
+        $("#headerImgRed").css({
+            display: "none",
+        })
+        $("#headerImgPink").css({
+            display: "none",
+        })
+        $("#headerImgGreen").css({
+            display: "block",
+        });
+        $(".navbar").css({
+            backgroundColor: "rgb(228 228 210)",
+        });
+        $(".navbar-collapse").css({
+            backgroundColor: "rgb(228 228 210)",
+        });
+        $("li a").css({
+            color: "black"
+        });
+        $(".navbar-brand").css({
+            color: "black",
+        });
+        $(".navbar-toggler").css({
+            background: "none",
+        });
+        $("#about_me").css({
+            backgroundColor: "rgb(183 109 102)",
+            color: "black"
+        });
+        $("#card1").css({
+            backgroundColor: "rgb(228 228 210)",
+            color: "black",
+        });
+        $("#card2").css({
+            backgroundColor: "rgb(228 228 210)",
+            color: "black",
+        });
+        $("#card3").css({
+            backgroundColor: "rgb(228 228 210)",
+            color: "black",
+        });
+        $("#card4").css({
+            backgroundColor: "rgb(228 228 210)",
+            color: "black",
+        });
+        $(".card-button").css({
+            backgroundColor: "rgb(54, 84, 44)",
+        });
+        $(".card-button").hover(function () {
+            $(this).css({
+                backgroundColor: "white",
+                color: "rgb(54, 84, 44)",
+                border: "0.2rem solid rgb(54, 84, 44)",
+            });
+        }, function () {
+            $(this).css({
+                backgroundColor: "rgb(54, 84, 44)",
+                color: "white",
+                border: "0rem solid black",
+            });
+        }
+        );
+        $(".form-button").css({
+            backgroundColor: "rgb(228 228 210)",
+            color: "black",
+        });
+        $(".form-button").hover(function () {
+            $(this).css({
+                backgroundColor: "white",
+                color: "rgb(54, 84, 44)",
+                border: "0.2rem solid rgb(54, 84, 44)",
+            });
+        }, function () {
+            $(this).css({
+                backgroundColor: "rgb(228 228 210)",
+                color: "black",
+                border: "0rem solid black",
+            });
+        }
+        );
+        $("#form-section").css({
+            backgroundColor: "rgb(183 109 102)",
+        });
+        $("#form-section h3").css({
+            color: "black",
+        });
+        $("#form-section hr").css({
+            color: "black",
+        });
+        $("#social").css({
+            backgroundColor: "rgb(228 228 210)",
+        });
+        $("#icons i").css({
+            color: "black",
+        });
+        $("#icons i").hover(function () {
+            $(this).css({
+                color: "rgb(183 109 102)",
+            });
+        }, function () {
+            $(this).css({
+                color: "black",
+            });
+        });
+        $("#copyright").css({
+            backgroundColor: "rgb(183 109 102)",
+        });
+
+    });
+
+
+
+    $("#blue").click(function () {
+        console.log("lala");
+        $("#body").css({
+            backgroundColor: "rgb(38 56 76)",
+        });
+        $("#headerImgRed").css({
+            display: "none",
+        });
+        $("#headerImgPink").css({
+            display: "none",
+        })
+        $("#headerImgGreen").css({
+            display: "none",
+        });
+        $("#headerImgBlue").css({
+            display: "block",
+        });
+        $("nav").css({
+            backgroundColor: "rgb(167, 192, 197)",
+        });
+        $(".navbar-collapse").css({
+            backgroundColor: "rgb(167, 192, 197)",
+        });
+        $("li a").css({
+            color: "black"
+        });
+        $(".navbar-brand").css({
+            color: "black",
+        });
+        $(".navbar-toggler").css({
+            background: "rgb(167, 192, 197)",
+        });
+        $("#about_me").css({
+            backgroundColor: "rgb(210 181 193)",
+            color: "black"
+        });
+        $("#card1").css({
+            backgroundColor: "rgb(167, 192, 197)",
+            color: "white",
+        });
+        $("#card2").css({
+            backgroundColor: "rgb(167, 192, 197)",
+            color: "white",
+        });
+        $("#card3").css({
+            backgroundColor: "rgb(167, 192, 197)",
+            color: "white",
+        });
+        $("#card4").css({
+            backgroundColor: "rgb(167, 192, 197)",
+            color: "white",
+        });
+        $(".card-button").css({
+            backgroundColor: "rgb(38 56 76)",
+        });
+        $(".card-button").hover(function () {
+            $(this).css({
+                backgroundColor: "white",
+                color: "rgb(38 56 76)",
+                border: "0.2rem solid rgb(38 56 76)",
+            });
+        }, function () {
+            $(this).css({
+                backgroundColor: "rgb(38 56 76)",
+                color: "white",
+                border: "0rem solid black",
+            });
+        }
+        );
+        $(".form-button").css({
+            backgroundColor: "rgb(38 56 76)",
+            color: "white",
+        });
+        $(".form-button").hover(function () {
+            $(this).css({
+                backgroundColor: "white",
+                color: "rgb(38 56 76)",
+                border: "0.2rem solid rgb(38 56 76)",
+            });
+        }, function () {
+            $(this).css({
+                backgroundColor: "rgb(38 56 76)",
+                color: "white",
+                border: "0rem solid black",
+            });
+        });
+        $("#form-section").css({
+            backgroundColor: "rgb(210 181 193)",
+        });
+        $("#form-section h3").css({
+            color: "black",
+        });
+        $("#form-section hr").css({
+            color: "black",
+        });
+        $("#social").css({
+            backgroundColor: "rgb(167, 192, 197)",
+        });
+        $("#icons i").css({
+            color: "rgb(38 56 76)",
+        });
+        $("#icons i").hover(function () {
+            $(this).css({
+                color: "white",
+            });
+        }, function () {
+            $(this).css({
+                color: "rgb(38 56 76)",
+            });
+        }
+        );
+        $("#copyright").css({
+            backgroundColor: "rgb(210 181 193)",
+            color: "rgb(38 56 76)",
+        });
+
+    });
+
+
+
+    $("#red").click(function () {
+        console.log("lala");
+        $("#body").css({
+            backgroundColor: "rgb(65 1 0)",
+        });
+        $("#headerImgBlue").css({
+            display: "none",
+        });
+        $("#headerImgPink").css({
+            display: "none",
+        })
+        $("#headerImgGreen").css({
+            display: "none",
+        });
+        $("#headerImgRed").css({
+            display: "block",
+        });
+        $("nav").css({
+            backgroundColor: "black",
+        });
+        $(".navbar-collapse").css({
+            backgroundColor: "black",
+        });
+        $("li a").css({
+            color: "white"
+        });
+        $(".navbar-brand").css({
+            color: "white",
+        });
+        $(".navbar-toggler").css({
+            background: "white",
+        });
+        $("#about_me").css({
+            backgroundColor: "black",
+            color: "white"
+        });
+        $("#card1").css({
+            backgroundColor: "white",
+            color: "black",
+        });
+        $("#card2").css({
+            backgroundColor: "white",
+            color: "black",
+        });
+        $("#card3").css({
+            backgroundColor: "white",
+            color: "black",
+        });
+        $("#card4").css({
+            backgroundColor: "white",
+            color: "black",
+        });
+        $(".card-button").css({
+            backgroundColor: "rgb(65 1 0)",
+        });
+        $(".card-button").hover(function () {
+            $(this).css({
+                backgroundColor: "white",
+                color: "rgb(65 1 0)",
+                border: "0.2rem solid rgb(65 1 0)",
+            });
+        }, function () {
+            $(this).css({
+                backgroundColor: "rgb(65 1 0)",
+                color: "white",
+                border: "0rem solid black",
+            });
+        });
+        $(".form-button").css({
+            backgroundColor: "rgb(65 1 0)",
+            color: "white",
+        });
+        $(".form-button").hover(function () {
+            $(this).css({
+                backgroundColor: "white",
+                color: "rgb(65 1 0)",
+                border: "0.2rem solid rgb(65 1 0)",
+            });
+        }, function () {
+            $(this).css({
+                backgroundColor: "rgb(65 1 0)",
+                color: "white",
+                border: "0rem solid black",
+            });
+        }
+        );
+        $("#form-section").css({
+            backgroundColor: "black",
+        });
+        $("#form-section h3").css({
+            color: "white",
+        });
+        $("#form-section hr").css({
+            color: "white",
+        });
+        $("#social").css({
+            backgroundColor: "white",
+        });
+        $("#icons i").css({
+            color: "black",
+        });
+        $("#icons i").hover(function () {
+            $(this).css({
+                color: "rgb(65 1 0)",
+            });
+        }, function () {
+            $(this).css({
+                color: "black",
+            });
+        }
+        );
+        $("#copyright").css({
+            backgroundColor: "black",
+            color: "white",
+        });
+
+    });
+
+
+
+    $("#pink").click(function () {
+        console.log("lala");
+        $("#body").css({
+            backgroundColor: "rgb(163 86 112)",
+        });
+        $("#headerImgRed").css({
+            display: "none",
+        });
+        $("#headerImgBlue").css({
+            display: "none",
+        })
+        $("#headerImgGreen").css({
+            display: "none",
+        });
+        $("#headerImgPink").css({
+            display: "block",
+        });
+        $("nav").css({
+            backgroundColor: "rgb(233 233 233)",
+        });
+        $(".navbar-collapse").css({
+            backgroundColor: "rgb(233 233 233)",
+        });
+        $("li a").css({
+            color: "black"
+        });
+        $(".navbar-brand").css({
+            color: "black",
+        });
+        $(".navbar-toggler").css({
+            background: "rgb(233 233 233)",
+        });
+        $("#about_me").css({
+            backgroundColor: "rgb(210 181 193)",
+            color: "black"
+        });
+        $("#card1").css({
+            backgroundColor: "rgb(233 233 233)",
+            color: "black",
+        });
+        $("#card2").css({
+            backgroundColor: "rgb(233 233 233)",
+            color: "black",
+        });
+        $("#card3").css({
+            backgroundColor: "rgb(233 233 233)",
+            color: "black",
+        });
+        $("#card4").css({
+            backgroundColor: "rgb(233 233 233)",
+            color: "black",
+        });
+        $(".card-button").css({
+            backgroundColor: "rgb(163 86 112)",
+        });
+        $(".card-button").hover(function () {
+            $(this).css({
+                backgroundColor: "white",
+                color: "rgb(163 86 112)",
+                border: "0.2rem solid rgb(163 86 112)",
+            });
+        }, function () {
+            $(this).css({
+                backgroundColor: "rgb(163 86 112)",
+                color: "white",
+                border: "0rem solid black",
+            });
+        }
+        );
+        $(".form-button").css({
+            backgroundColor: "rgb(163 86 112)",
+            color: "white",
+        });
+        $(".form-button").hover(function () {
+            $(this).css({
+                backgroundColor: "white",
+                color: "rgb(163 86 112)",
+                border: "0.2rem solid rgb(163 86 112)",
+            });
+        }, function () {
+            $(this).css({
+                backgroundColor: "rgb(163 86 112)",
+                color: "white",
+                border: "0rem solid black",
+            });
+        });
+        $("#form-section").css({
+            backgroundColor: "rgb(210 181 193)",
+        });
+        $("#form-section h3").css({
+            color: "black",
+        });
+        $("#form-section hr").css({
+            color: "black",
+        });
+        $("#social").css({
+            backgroundColor: "rgb(233 233 233)",
+        });
+        $("#icons i").css({
+            color: "black",
+        });
+        $("#icons i").hover(function () {
+            $(this).css({
+                color: "rgb(210 181 193)",
+            });
+        }, function () {
+            $(this).css({
+                color: "black",
+            });
+        }
+        );
+        $("#copyright").css({
+            backgroundColor: "rgb(210 181 193)",
+            color: "black",
+        });
+
+    });
+});
