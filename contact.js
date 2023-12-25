fb5c11ff54ea079f90b7e12a2660102395fa70b6
melikeeros <orsmelike2003@gmail.com>
2023-12-25 22:31:14 +0300

    form js ozelligi duzenlendi (input alma tamamlandi, mail dogrulama sorunu)

--- contact.js
+++ contact.js
@@ -1,14 +1,32 @@
+let formDOM = document.querySelector("#form-container");
+formDOM.addEventListener("submit", formSubmit);
+
+var emailInput = document.querySelector("#floatingInput")
+  var subjectInput = document.querySelector("#floatingPassword")
+  var commentsInput = document.querySelector("#floatingTextarea2")
+
+function formSubmit(event) {
+  console.log("denemeee")
+  console.log(emailInput.value)
+  console.log(subjectInput.value)
+  console.log(commentsInput.value)
+}
+
 function send() {
-    console.log("deneme")
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "orsmelike2003@gmail.com",
-        Password : "F4DCBA918D93FB9EADCF6AA471B4858F371B",
-        To : 'orsmelike2003@gmail.com',
-        From : "orsmelike2003@gmail.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
-    }).then(
-      message => alert(message)
-    );
+  console.log("deneme")
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username : "orsmelike2003@gmail.com",
+    Password : "F4DCBA918D93FB9EADCF6AA471B4858F371B",
+    From: emailInput.value,
+    To: 'orsmelike2003@gmail.com',
+    
+    Subject: subjectInput.value,
+    Body: commentsInput.value
+  }).then(
+    message => {
+      alert("mail sent successfully.");
+      console.log(message);
+    }
+  );
 }
